e2523d11fc639e492d5078a13788e33f6791eeba
Nikita\WILYR <wilyrjvdv@gmail.com>
2020-09-03 07:45:53 +0300

    Task's rework

--- src/src/main/java/com/wilyr/javacore/chapter18/tasks/task1.java
+++ src/src/main/java/com/wilyr/javacore/chapter18/tasks/task1.java
@@ -15,13 +15,14 @@ public class task1 {
         Iterator<Integer> itr = adq.iterator();
         Iterator<Integer> itr2 = adq.iterator();
         Integer element, element2, tmp = null, tmp2 = null, count = 0;
-        while (itr.hasNext()) {
+        exitLabel: while (itr.hasNext()) {
             element = itr.next();
             while (itr2.hasNext()) {
                 element2 = itr2.next();
                 if (!element.equals(element2) && element + element2 == number) {
                     tmp = element;
                     tmp2 = element2;
+                    break exitLabel;
                 }
             }
             itr2 = adq.iterator();
